3f7ed9859e6e53e50aedd20cc626ad5faee825f4
Henry.Wang <Henry.Wang@fanruan.com>
2023-08-29 17:23:37 +0800

    feat: 提交代码 #none

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,67 @@ export function formate(
   formatter: string | ((dateInfo: DateInfo) => string),
   isPad = false
 ): string {
-  return "";
+  const dateInfo = getDateInfo(date);
+  if (typeof formatter == "string") {
+    const template = getTemplate(formatter);
+    return formateByTemplate(dateInfo, template, isPad);
+  }
+  return formatter(dateInfo);
 }
+const getDateInfo = (date: Date): DateInfo => {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  return {
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second,
+    yyyy: `${year}`,
+    MM: `${month + 1}`,
+    dd: `${day}`,
+    hh: `${hour}`,
+    mm: `${minute}`,
+    ss: `${second}`,
+  };
+};
+
+const getTemplate = (formatter: string) => {
+  let template = null;
+  if (formatter === "date") {
+    template = "yyyy-MM-dd";
+  } else if (formatter === "datetime") {
+    template = "yyyy-MM-dd hh:mm:ss";
+  } else {
+    template = formatter;
+  }
+  return template;
+};
+
+const formateByTemplate = (
+  dateInfo: DateInfo,
+  template: string,
+  isPad = false
+) => {
+  const getPadValue = (rawValue: string, length: number) => {
+    const padLength = Math.max(0, length - rawValue.length);
+    return "0".repeat(padLength) + rawValue;
+  };
+  return [
+    "yyyy" as const,
+    "MM" as const,
+    "dd" as const,
+    "hh" as const,
+    "mm" as const,
+    "ss" as const,
+  ].reduce((pre, cur) => {
+    const rawValue = dateInfo[cur];
+    const resultValue = isPad ? getPadValue(rawValue, cur.length) : rawValue;
+    return pre.replace(cur, resultValue);
+  }, template);
+};
